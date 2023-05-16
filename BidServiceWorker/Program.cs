dbd519197edbaab4f2c39906768dcedd154a83ea
danieldawsonjensen <dani540c@gmail.com>
2023-05-16 11:13:19 +0200

    first commit

---
+++ BidServiceWorker/Program.cs
@@ -0,0 +1,10 @@
+using BidServiceWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
