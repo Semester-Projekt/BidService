631a2e9167f37ee5624b4ccbf7977b872e346ee4
danieldawsonjensen <dani540c@gmail.com>
2023-05-20 14:40:44 +0200

    få ændringer i setup

--- BidServiceWorker/Worker.cs
+++ BidServiceWorker/Worker.cs
@@ -1,13 +1,52 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+
 namespace BidServiceWorker;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _config;
 
-    public Worker(ILogger<Worker> logger)
+
+    public Worker(ILogger<Worker> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
+
+        
     }
+
+    
+
+
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
